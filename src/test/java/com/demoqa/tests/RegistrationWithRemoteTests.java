b29d000c2d2bebe494a8bbb1fe060aae0585ef42
Denis Litinskiy <plnvoran@mail.ru>
2023-05-07 10:53:15 +0300

    Сделал ДЗ 12.

---
+++ src/test/java/com/demoqa/tests/RegistrationWithRemoteTests.java
@@ -0,0 +1,40 @@
+package com.demoqa.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.demoqa.data.FieldsForRegistrationTestResult.*;
+import static com.demoqa.data.ValuesForRegistrationsTest.*;
+
+public class RegistrationWithRemoteTests extends RemoteTestBase {
+
+    @Test
+    void successfulRegistrationTest() {
+        registrationPage.openPage()
+                .removeBanners()
+                .setFirstName(nameValue)
+                .setLastName(lastNameValue)
+                .setUserEmail(emailValue)
+                .setGender(genderValue)
+                .setNumber(numberValue)
+                .setBirthDate (dayValue, monthValue, yearValue)
+                .setSubjects(subjectValue)
+                .setHobbies(hobbyValue)
+                .uploadPicture(pathValue, fileValue)
+                .setCurrentAddress(addressValue)
+                .setState(stateValue)
+                .setCity(cityValue)
+                .clickSubmit();
+
+        registrationPage.verifyRegistrationResultsModalAppears()
+                .verifyResult(studentNameField, nameValue + " " + lastNameValue)
+                .verifyResult(studentEmailField, emailValue)
+                .verifyResult(genderField, genderValue)
+                .verifyResult(mobileField, numberValue)
+                .verifyResult(dateBirthField, dayValue + " " + monthValue + "," + yearValue)
+                .verifyResult(subjectsField, subjectValue)
+                .verifyResult(hobbiesField, hobbyValue)
+                .verifyResult(pictureField, fileValue)
+                .verifyResult(addressField, addressValue)
+                .verifyResult(stateCityField, stateValue + " " + cityValue);
+    }
+}
