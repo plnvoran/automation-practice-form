085b99d927ea6ef299510fe3667e7aee3321161c
Denis Litinskiy <plnvoran@mail.ru>
2023-11-28 13:09:05 +0300

    hh

--- src/test/java/com/demoqa/AutomationPracticeFormTest.java
+++ src/test/java/com/demoqa/AutomationPracticeFormTest.java
@@ -2,73 +2,25 @@ package com.demoqa;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AutomationPracticeFormTest extends TestBase {
-    String UrlAutomationPracticeForm = "/automation-practice-form";
-    String firstNameTestValue = "John";
-    String lastNameTestValue = "Smith";
-    String emailTestValue = "JohnSmith76@gmail.com";
-    String genderTestValue = "Male";
-    String userNumberTestValue = "9221234567";
-    String hobbiesTestValue = "Sports";
-    String monthTestValue = "May";
-    String yearTestValue = "2005";
-    String weekTestValue = ".react-datepicker__month>div:nth-child(3)>.react-datepicker__day--0";
+    String UrlAutomationPracticeForm = "";
+    String monthTestValue = "январь";
     String dayTestValue = "15";
-    String subjectTestValue = "Maths";
-    String addressTestValue = "Address India 04";
-    String stateTestValue = "NCR";
-    String cityTestValue = "Delhi";
-    String imageDirTestValue = "src/test/resources/";
-    String imageTestValue = "CharlesBukowski.jpg";
 
     @Test
-    void successfulFillFormTest() {
+    void getDayTest() {
 
         open(UrlAutomationPracticeForm);
-        executeJavaScript("$('footer').remove()");
-        executeJavaScript("$('#fixedban').remove()");
-        $(".main-header").shouldHave(text("Practice Form"));
 
-        $("#firstName").setValue(firstNameTestValue);
-        $("#lastName").setValue(lastNameTestValue);
-        $("#userEmail").setValue(emailTestValue);
-        $("#genterWrapper").$(byText(genderTestValue)).click();
-        $("#userNumber").setValue(userNumberTestValue);
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").$(byText(monthTestValue)).click();
-        $(".react-datepicker__year-select").$(byText(yearTestValue)).click();
-        $(weekTestValue + dayTestValue).click();
-        $("#subjectsContainer input").setValue(subjectTestValue).pressEnter();
-        $("#hobbiesWrapper").$(byText(hobbiesTestValue)).click();
-        $("#uploadPicture").uploadFile(new File(imageDirTestValue + imageTestValue));
-        $("#currentAddress").setValue(addressTestValue);
-        $("#state").click();
-        $("#stateCity-wrapper").$(byText(stateTestValue)).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText(cityTestValue)).click();
+    $$("//th[contains(text(),'" );
 
-        $("#submit").click();
+//.ektHAse___chat-item-info-subject
+               // .uNmi60c___chat-item-info-top
 
-        $(".table-responsive").shouldHave(visible);
-        $(".table-responsive").shouldHave(
-                text(firstNameTestValue),
-                text(lastNameTestValue),
-                text(emailTestValue),
-                text(genderTestValue),
-                text(userNumberTestValue),
-                text(dayTestValue + " " + monthTestValue + "," + yearTestValue),
-                text(subjectTestValue),
-                text(hobbiesTestValue),
-                text(imageTestValue),
-                text(addressTestValue),
-                text(stateTestValue + " " + cityTestValue)
-        );
+
     }
 }
 
--- src/test/java/com/demoqa/TestBase.java
+++ src/test/java/com/demoqa/TestBase.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.baseUrl = "///E:/_Profession/HH/%D0%9E%D1%82%D0%BA%D0%BB%D0%B8%D0%BA%D0%B8%20%D0%B8%20%D0%BF%D1%80%D0%B8%D0%B3%D0%BB%D0%B0%D1%88%D0%B5%D0%BD%D0%B8%D1%8F.html";
         Configuration.browserSize = "1920x1080";
     }
 }
